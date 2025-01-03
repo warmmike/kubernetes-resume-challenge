025efbbc9a3eaa61f143f04637d6c6170accc0fb
warmmike <mcarman@gmail.com>
2025-01-03 14:38:24 -0500

    "Add Helm provider with Kubernetes credentials"

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,19 @@
 # VPC for Cluster
 data "aws_availability_zones" "azs" {}
 
+data "aws_eks_cluster_auth" "cluster" {
+  depends_on = [module.eks]
+  name = module.eks.cluster_name
+}
+
+provider "helm" {
+  kubernetes {
+    host = module.eks.cluster_endpoint
+    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+    token = data.aws_eks_cluster_auth.cluster.token
+  }
+}
+
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "~> 5.0"
