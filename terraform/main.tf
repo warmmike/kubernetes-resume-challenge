8312070c9251cf756f2bfedf40a452d7a2cabf35
warmmike <mcarman@gmail.com>
2025-01-03 19:06:55 -0500

    "Try lb_role module for Load Balancer Controller permissions"

--- terraform/main.tf
+++ terraform/main.tf
@@ -81,22 +81,6 @@ module "eks" {
   tags = var.tags
 }
 
-data "aws_iam_policy_document" "aws_lbc" {
-  statement {
-    effect = "Allow"
-
-    principals {
-      type        = "Service"
-      identifiers = ["pods.eks.amazonaws.com"]
-    }
-
-    actions = [
-      "sts:AssumeRole",
-      "sts:TagSession"
-    ]
-  }
-}
-
 module "lb_role" {
   source    = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
 
