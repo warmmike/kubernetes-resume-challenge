dd9ca10ce5dd5d887addb866c25a63fcf5e5b7ad
warmmike <mcarman@gmail.com>
2025-01-03 20:48:36 -0500

    "Fix chart urls"

--- terraform/main.tf
+++ terraform/main.tf
@@ -95,3 +95,18 @@ module "lb_role" {
     }
   }
 }
+
+resource "kubernetes_service_account" "service-account" {
+  metadata {
+    name = "aws-load-balancer-controller"
+    namespace = "kube-system"
+    labels = {
+        "app.kubernetes.io/name"= "aws-load-balancer-controller"
+        "app.kubernetes.io/component"= "controller"
+    }
+    annotations = {
+      "eks.amazonaws.com/role-arn" = module.lb_role.arn
+      "eks.amazonaws.com/sts-regional-endpoints" = "true"
+    }
+  }
+}
