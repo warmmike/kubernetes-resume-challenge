a25c1ab77405fef4583065f7cb702b59f8f31ad8
warmmike <mcarman@gmail.com>
2025-01-06 21:17:51 -0500

    "Increase worker node size and wait for Helm uninstall during destroy - fix sg during destroy"

--- terraform/main.tf
+++ terraform/main.tf
@@ -63,7 +63,7 @@ module "eks" {
 
   eks_managed_node_groups = {
     initial = {
-      instance_types = ["t3.small"]
+      instance_types = ["t3.medium"]
       min_size       = 2
       max_size       = 4
       desired_size   = 2
